def5923a2d0abed9968bc11b5eb8a6bb6d8f9230
Oleksandr <it.appatit@gmail.com>
2020-11-29 18:12:15 +0200

    Must done carousel by slick. ont did dots

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick();
+    $('.carousel__inner').slick({
+      speed: 1200,
+      // adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"> <img src="../ico/chevron-left-solid.svg"</button>',
+      nextArrow: '<button type="button" class="slick-next"> <img src="../ico/chevron-right-solid.svg"</button>',
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            // infinite: true,
+            dots: true
+          }
+        },
+      ]
+    });
   });
