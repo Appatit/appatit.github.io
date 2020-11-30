e5b8a15ab92abd3f38f8411752a3b9e28555871f
Oleksandr <it.appatit@gmail.com>
2020-11-30 09:24:54 +0200

    Slider ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,11 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-      speed: 1200,
-      // adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"> <img src="../ico/chevron-left-solid.svg"</button>',
-      nextArrow: '<button type="button" class="slick-next"> <img src="../ico/chevron-right-solid.svg"</button>',
-      responsive: [
-        {
-          breakpoint: 992,
-          settings: {
-            // infinite: true,
-            dots: true
-          }
-        },
-      ]
-    });
-  });
+var slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    nav: false,
+    controlsText: [
+      '<img src="../ico/chevron-left-solid.svg" alt="">',
+      '<img src="../ico/chevron-right-solid.svg" alt="">',
+    ]
+});
